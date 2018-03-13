a681b03cbf8d69d0cd55e1db1982c7d7854c6300
Tom Westbrook <tom@tomwestbrook.com>
2018-03-12 19:29:52 -0500

    First version

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
+import unittest
 
 browser = webdriver.Firefox()
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+assert 'To-Do' in browser.title
+
+browser.quit()
